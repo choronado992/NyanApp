8cbc0e36285f3214abc42bacc152b3a025eb01f1
Christie Etter <christie.etter@gmail.com>
2024-01-03 22:48:11 -0600

    Add NyanCat video and player.

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,6 +16,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo()
+    }
+    
+    private func playVideo(){
+        guard let path=Bundle.main.path(forResource: "Nyan Cat [original]", ofType:"mp4") else {
+                    debugPrint("test.mp4 not found")
+                    return
+                }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player=player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
 
 }
 
